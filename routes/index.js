febc14fb4262b508b115072d35a70cf77cf04639
Paul Ingalls <paulingalls@hotmail.com>
2012-02-28 22:26:34 -0800

    added connection to pg

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,26 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+	
+  var pg = require('pg');
+  var theDBUrl = process.env.DATABASE_URL || "tcp://cms:cms@localhost/cms";
+
+  pg.connect( theDBUrl, function(err, client) {
+    if (err)
+    {
+       console.log(err);
+       res.render('index', { title: 'Express' + JSON.stringify(err) });
+       return;
+    }
+
+    var query = client.query('SELECT * FROM sport');
+
+    query.on('row', function(row) {
+      res.render('index', { title: 'Express' + JSON.stringify(row) });
+    });
+  });
+
 };
+
+
+
