ab324a568c7a8ed77b5eb29af67217919304d848
Paul Ingalls <paul@paulingalls.com>
2012-04-27 18:21:43 -0700

    work with new DB

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@
 exports.index = function(req, res){
 	
   var pg = require('pg');
-  var theDBUrl = process.env.DATABASE_URL || "tcp://cms:cms@localhost/cms";
+  var theDBUrl = process.env.DATABASE_URL || "tcp://fanzo_site:fanzo_site@localhost/fanzo_site_development";
 
   pg.connect( theDBUrl, function(err, client) {
     if (err)
@@ -16,7 +16,7 @@ exports.index = function(req, res){
        return;
     }
 
-    var query = client.query('SELECT * FROM sport');
+    var query = client.query('SELECT * FROM sports');
 
     query.on('row', function(row) {
       res.render('index', { title: 'Express' + JSON.stringify(row) });
